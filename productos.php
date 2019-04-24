16d7b252799186e0176e2cc331be8ce87fca3695
Cristian Camilo Giraldo <cristian_camilo1505@hotmail.com>
2019-04-23 23:35:38 -0500

    correcciones

--- productos.php
+++ productos.php
@@ -96,29 +96,10 @@
             <div class="card" style="width: 18rem;">
               <img class="card-img-top" src="<?php print($producto["imagen"]); ?>" alt="Card image cap" height="200" width="400">
               <div class="card-body">
-                <p class="card-title"> <?php print($producto["nombre_producto"]); ?> </p>
-                <p> $<?php print($producto["costo"]); ?> </p>
+                <h5 class="card-title"> <?php print($producto["nombre_producto"]); ?> </h5>
+                <p class="card-text">Referencia: <?php print($producto["referencia"]); ?> </p>
+                <p class="card-text"> $<?php print( number_format($producto["costo"], $decimals = 0, $dec_point = ",", $thousands_sep = "." )); ?> </p>
                 <p class="card-text"> <?php print($producto["descripcion"]); ?> </p>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Detalles</button>
-              </div>
-            </div>
-          </div>
-          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  ...
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
               </div>
             </div>
           </div>
