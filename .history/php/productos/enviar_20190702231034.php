99ba8acf18eaa9d63f2e3572b6f5149e847d73fb
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-07-02 23:10:57 -0500

    ajuste

---
+++ .history/php/productos/enviar_20190702231034.php
@@ -0,0 +1,24 @@
+<?php
+    include("../BDservices.php");
+    $producto = $_POST["nombre_producto"];
+
+    $username = "jp";
+    $password = "1234";
+    $consulta = "INSERT INTO producto(nombre_producto) VALUES('$producto')";
+    $respuesta = setData($consulta, $username, $password);
+    if($respuesta){
+        echo "
+        <script>
+            alert('Guardado Correctamente');
+            window.location.href='addProducto.php';
+        </script>
+    ";
+    }else{
+        echo "
+        <script>
+            alert('Error Guardando el Producto');
+            window.location.href='addProducto.php';
+        </script>
+    ";
+    }
+?>
